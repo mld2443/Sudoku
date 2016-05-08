86168d9eb3d288967a60bde4380522745283bb6b
Matthew Dillard <7395051@gmail.com>
2016-05-07 22:44:32 -0500

    Initial Commit

---
+++ Sudoku/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sudoku
+//
+//  Created by Matthew Dillard on 5/7/16.
+//  Copyright © 2016 Matthew Dillard. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
