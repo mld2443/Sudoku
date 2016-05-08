a37f44f6d13bf6e53846469d6ed3865c0717e9f9
Matthew Dillard <7395051@gmail.com>
2016-05-08 10:12:40 -0500

    Added board jumbling
    * Added 8 different ways to generate permutations of a given sudoku
    board
    * Added a function to apply all of them randomly
    * wasted a lot of time on unit testing and multithreading, neither of
    which lead anywhere

--- Sudoku/Sudoku.swift
+++ Sudoku/Sudoku.swift
@@ -10,35 +10,162 @@ import Foundation
 
 
 /// Holds a Sudoku grid and many useful operations for that grid
-struct Sudoku {
+public struct Sudoku {
 	var grid = [[UInt8]](count: 9, repeatedValue:[UInt8](count: 9, repeatedValue:0))
+}
+
+
+// Permutations to a board
+extension Sudoku{
+	public mutating func jumble(seed: UInt32 = UInt32(time(nil))) {
+		srandom(seed)
+		
+		jumbleInnerRows()
+		jumbleOuterRows()
+		jumbleInnerColumns()
+		jumbleOuterColumns()
+		jumbleNumbers()
+		
+		rotateClockwise(random() % 4)
+		
+		if random() % 2 == 1 {
+			mirrorHorizontal()
+		}
+		
+		if random() % 2 == 1 {
+			mirrorVertical()
+		}
+	}
 	
-	var emptySpaces: Int {
-		var count = 0
+	public mutating func jumbleNumbers() {
+		var cipher = createCipher(9)
+		cipher.insert(0, atIndex: 0)
+		
+		var newGrid = [[UInt8]]()
 		
 		for row in grid {
-			for value in row where value == 0 {
-				count = count + 1
+			var newRow = [UInt8]()
+			for entry in row {
+				newRow.append(UInt8((entry > 0) ? cipher[Int(entry)] + 1 : 0))
+			}
+			newGrid.append(newRow)
+		}
+		
+		grid = newGrid
+	}
+	
+	public mutating func jumbleInnerRows() {
+		let cipher1 = createCipher(3)
+		let cipher2 = createCipher(3)
+		let cipher3 = createCipher(3)
+		
+		var newGrid = [[UInt8]](count: 9, repeatedValue: [UInt8]())
+		
+		for index in 0...2 {
+			newGrid[index + 0] = grid[cipher1[index] + 0]
+			newGrid[index + 3] = grid[cipher2[index] + 3]
+			newGrid[index + 6] = grid[cipher3[index] + 6]
+		}
+		
+		grid = newGrid
+	}
+	
+	public mutating func jumbleInnerColumns() {
+		let cipher1 = createCipher(3)
+		let cipher2 = createCipher(3)
+		let cipher3 = createCipher(3)
+		
+		var newGrid = [[UInt8]]()
+		
+		for row in grid {
+			var newRow = [UInt8](count: 9, repeatedValue: 0)
+			
+			for index in 0...2 {
+				newRow[index + 0] = row[cipher1[index] + 0]
+				newRow[index + 3] = row[cipher2[index] + 3]
+				newRow[index + 6] = row[cipher3[index] + 6]
+			}
+			
+			newGrid.append(newRow)
+		}
+		
+		grid = newGrid
+	}
+	
+	public mutating func jumbleOuterRows() {
+		let cipher = createCipher(3)
+		
+		var newGrid = [[UInt8]](count: 9, repeatedValue: [UInt8]())
+		
+		for index in 0...2 {
+			newGrid[3 * index + 0] = grid[3 * cipher[index] + 0]
+			newGrid[3 * index + 1] = grid[3 * cipher[index] + 1]
+			newGrid[3 * index + 2] = grid[3 * cipher[index] + 2]
+		}
+		
+		grid = newGrid
+	}
+	
+	public mutating func jumbleOuterColumns() {
+		let cipher = createCipher(3)
+		
+		var newGrid = [[UInt8]]()
+		
+		for row in grid {
+			var newRow = [UInt8](count: 9, repeatedValue: 0)
+			
+			for index in 0...2 {
+				newRow[3 * index + 0] = row[3 * cipher[index] + 0]
+				newRow[3 * index + 1] = row[3 * cipher[index] + 1]
+				newRow[3 * index + 2] = row[3 * cipher[index] + 2]
+			}
+			
+			newGrid.append(newRow)
+		}
+		
+		grid = newGrid
+	}
+	
+	public mutating func rotateClockwise(rotations: Int = 1) {
+		let xRotations = [{(y: Int, x: Int) in x}, {(y: Int, x: Int) in y}, {(y: Int, x: Int) in 8 - x}, {(y: Int, x: Int) in 8 - y}]
+		let yRotations = [{(y: Int, x: Int) in y}, {(y: Int, x: Int) in 8 - x}, {(y: Int, x: Int) in 8 - y}, {(y: Int, x: Int) in x}]
+		
+		let mutX = xRotations[rotations % 4]
+		let mutY = yRotations[rotations % 4]
+		
+		var newGrid = [[UInt8]](count: 9, repeatedValue:[UInt8](count: 9, repeatedValue:0))
+		
+		for y in 0...8 {
+			for x in 0...8 {
+				newGrid[y][x] = grid[mutY(y,x)][mutX(y,x)]
 			}
 		}
 		
-		return count
+		grid = newGrid
 	}
 	
-	func getCluster(x: Int, _ y: Int) -> [UInt8] {
-		var cluster = [UInt8]()
+	public mutating func mirrorHorizontal() {
+		var newGrid = [[UInt8]](count: 9, repeatedValue:[UInt8](count: 9, repeatedValue:0))
 		
-		cluster.append(grid[3*x+0][3*y+0])
-		cluster.append(grid[3*x+1][3*y+0])
-		cluster.append(grid[3*x+2][3*y+0])
-		cluster.append(grid[3*x+0][3*y+1])
-		cluster.append(grid[3*x+1][3*y+1])
-		cluster.append(grid[3*x+2][3*y+1])
-		cluster.append(grid[3*x+0][3*y+2])
-		cluster.append(grid[3*x+1][3*y+2])
-		cluster.append(grid[3*x+2][3*y+2])
+		for y in 0...8 {
+			for x in 0...8 {
+				newGrid[y][x] = grid[y][8 - x]
+			}
+		}
 		
-		return cluster
+		grid = newGrid
+	}
+	
+	public mutating func mirrorVertical() {
+		var newGrid = [[UInt8]](count: 9, repeatedValue:[UInt8](count: 9, repeatedValue:0))
+		
+		for y in 0...8 {
+			for x in 0...8 {
+				newGrid[y][x] = grid[8 - y][x]
+			}
+		}
+		
+		grid = newGrid
 	}
 }
 
@@ -47,28 +174,7 @@ struct Sudoku {
 extension Sudoku {
 	/// - returns: Number of unique solutions in the current puzzle configuration
 	@warn_unused_result
-	func solveDFS() -> Int {
-		/// - returns: An array of all valid values for a given empty space in the grid
-		/// - note: this func assumes there are no duplicates
-		@warn_unused_result
-		func getValidMovesFor(position: (row: Int, col: Int)) -> Set<UInt8> {
-			func getValidMovesFor(group: [UInt8]) -> Set<UInt8> {
-				var valid: Set<UInt8> = [1,2,3,4,5,6,7,8,9]
-				
-				for value in group where value > 0 {
-					valid.remove(value)
-				}
-				
-				return valid
-			}
-			
-			let rowValid = getValidMovesFor(grid[position.row])
-			let colValid = getValidMovesFor(grid.map({$0[position.col]}))
-			let clusterValid = getValidMovesFor(getCluster(position.row / 3, position.col / 3))
-			
-			return rowValid.intersect(colValid.intersect(clusterValid))
-		}
-		
+	public func solveDFS() -> Int {
 		var solutions = 0
 		var moves = Set<UInt8>()
 		var coords = (0, 0)
@@ -107,26 +213,11 @@ extension Sudoku {
 	}
 }
 
+
+// For checking properties of a configuration
 extension Sudoku {
 	/// - returns: `True` if the puzzle configuration does not violate any Sudoku rules
-	var checkValidity: Bool {
-		/// Helper function to check groups for duplicate values
-		func checkGroup(group: [UInt8]) -> Bool {
-			var seen = [Bool](count: 9, repeatedValue: false)
-			
-			for value in group where value > 0 {
-				let index = Int(value - 1)
-				
-				if seen[index] {
-					return true
-				}
-				
-				seen[index] = true
-			}
-			
-			return false
-		}
-		
+	public var checkValidity: Bool {
 		// Check the rows
 		for index in 0...8 {
 			let row = grid[index]
@@ -158,12 +249,38 @@ extension Sudoku {
 		
 		return true
 	}
+	
+	/// - returns: number of empty spaces remaining in the puzzle
+	public var emptySpaces: Int {
+		var count = 0
+		
+		for row in grid {
+			for value in row where value == 0 {
+				count = count + 1
+			}
+		}
+		
+		return count
+	}
+	
+	/// - returns: number of taken spaces remaining in the puzzle
+	public var takenSpaces: Int {
+		var count = 0
+		
+		for row in grid {
+			for value in row where value > 0 {
+				count = count + 1
+			}
+		}
+		
+		return count
+	}
 }
 
 
-/// For printing out the puzzle
+// For printing the puzzle
 extension Sudoku : CustomStringConvertible {
-	var description : String {
+	public var description : String {
 		var descriptor = ""
 		
 		for row in 0...8 {
@@ -191,5 +308,89 @@ extension Sudoku : CustomStringConvertible {
 		}
 		
 		return descriptor
+	}
+}
+
+
+// Private functions
+extension Sudoku {
+	/// - returns: A ceaser cipher of requested size
+	private func createCipher(size: Int) -> [Int] {
+		var cipher = [Int](count: size, repeatedValue: 0)
+		
+		for range in (0..<size).reverse() {
+			var index = 0, position = random() % (range + 1)
+			
+			while index <= position {
+				if cipher[index] > 0 {
+					position = position + 1
+				}
+				
+				index = index + 1
+			}
+			
+			cipher[position] = range
+		}
+		
+		return cipher
+	}
+	
+	/// - parameters:
+	///   - Int: row of the 3x3 desired cluster
+	///   - Int: column of the 3x3 desired cluster
+	/// - returns: The group of values in the specified Cluster
+	@warn_unused_result
+	private func getCluster(x: Int, _ y: Int) -> [UInt8] {
+		var cluster = [UInt8]()
+		
+		cluster.append(grid[3*x+0][3*y+0])
+		cluster.append(grid[3*x+1][3*y+0])
+		cluster.append(grid[3*x+2][3*y+0])
+		cluster.append(grid[3*x+0][3*y+1])
+		cluster.append(grid[3*x+1][3*y+1])
+		cluster.append(grid[3*x+2][3*y+1])
+		cluster.append(grid[3*x+0][3*y+2])
+		cluster.append(grid[3*x+1][3*y+2])
+		cluster.append(grid[3*x+2][3*y+2])
+		
+		return cluster
+	}
+	
+	/// - returns: An array of all valid values for a given empty space in the grid
+	/// - note: this func assumes there are no duplicates
+	@warn_unused_result
+	public func getValidMovesFor(position: (row: Int, col: Int)) -> Set<UInt8> {
+		func getValidMovesForGroup(group: [UInt8]) -> Set<UInt8> {
+			var valid: Set<UInt8> = [1,2,3,4,5,6,7,8,9]
+			
+			for value in group where value > 0 {
+				valid.remove(value)
+			}
+			
+			return valid
+		}
+		
+		let rowValid = getValidMovesForGroup(grid[position.row])
+		let colValid = getValidMovesForGroup(grid.map({$0[position.col]}))
+		let clusterValid = getValidMovesForGroup(getCluster(position.row / 3, position.col / 3))
+		
+		return rowValid.intersect(colValid.intersect(clusterValid))
+	}
+	
+	/// Helper function to check groups for duplicate values
+	private func checkGroup(group: [UInt8]) -> Bool {
+		var seen = [Bool](count: 9, repeatedValue: false)
+		
+		for value in group where value > 0 {
+			let index = Int(value - 1)
+			
+			if seen[index] {
+				return true
+			}
+			
+			seen[index] = true
+		}
+		
+		return false
 	}
 }
